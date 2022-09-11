92ef674807e0db3728797de33c298ced8201fc17
francorinque <francoagustinrinquecorreo@gmail.com>
2022-09-11 18:00:10 -0300

    seccion about

--- js/utils.js
+++
@@ -1,6 +0,0 @@
-const $inputName = document.getElementById('nombre'),
-      $inputApellido = document.getElementById('apellido'),
-      $inputCorreo = document.getElementById('correo'),
-      $inputTelefono = document.getElementById('telefono');
-
-      
