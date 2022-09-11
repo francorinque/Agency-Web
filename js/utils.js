1e5b92d86aa45b54efe1a43cd342e5891502e782
francorinque <francoagustinrinquecorreo@gmail.com>
2022-09-10 21:27:28 -0300

    hero

---
+++ js/utils.js
@@ -0,0 +1,6 @@
+const $inputName = document.getElementById('nombre'),
+      $inputApellido = document.getElementById('apellido'),
+      $inputCorreo = document.getElementById('correo'),
+      $inputTelefono = document.getElementById('telefono');
+
+      
