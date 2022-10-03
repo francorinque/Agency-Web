8f029d1a5e73e73b247d84194efb21d249aad13c
francorinque <francoagustinrinquecorreo@gmail.com>
2022-10-03 15:46:10 -0300

    services

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,14 @@ const d = document;
 
 
 export default function menuToggle(btn,menu){
+    // traigo al elemento
     const $toggle = d.querySelector(btn),
     $menu = d.querySelector(menu);
-
+    // evento 
     d.addEventListener("click",  e => {
+        // delego el evento
         if(e.target.matches(btn) || e.target.matches(`${btn} *`)){
+            // añado la clase al menu
             $menu.classList.toggle("showMenu")
         }
     })
